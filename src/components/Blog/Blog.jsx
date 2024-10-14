b09ae9445955bb3d1fd3c115fd60ec88bb3a675f
maruf908548 <ahmedmaruf908548@gmail.com>
2024-10-14 13:45:05 +0600

    Bookmark Button handler added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,23 @@
 import PropTypes from "prop-types";
-const Blog = ({ blog }) => {
-  const { title, cover, author, author_img, posted_date, reading_time } = blog;
+import { FaBookmark } from "react-icons/fa";
+const Blog = ({ blog, handleAddToBookmark }) => {
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
   return (
-    <div>
-      <img src={cover} alt={`cover picture of the title ${title}`} />
-      <div className="flex justify-between">
+    <div className="mb-20">
+      <img
+        className="w-full mb-3"
+        src={cover}
+        alt={`cover picture of the title ${title}`}
+      />
+      <div className="flex justify-between mb-4">
         <div className="flex">
           <img className="w-14 rounded-full" src={author_img} alt="" />
           <div className="ml-6">
@@ -14,9 +27,19 @@ const Blog = ({ blog }) => {
         </div>
         <div>
           <span>{reading_time} min read</span>
+          <button onClick={handleAddToBookmark} className="ml-2 text-red-200">
+            <FaBookmark></FaBookmark>
+          </button>
         </div>
       </div>
       <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">#{hash}</a>
+          </span>
+        ))}
+      </p>
     </div>
   );
 };
